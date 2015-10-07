9bfc3029a20809620533d304b1d8631205c2e85e
Shawn Sandy <shawnsandy04@gmail.com>
2015-10-07 11:34:14 -0400

    refactoring webpack
    added babelrc — configuration
    renames old webpack config
    changed the scripts dev path

--- sample.webpack.config.js
+++ sample.webpack.config.js
@@ -3,44 +3,42 @@ var UglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
 var env = process.env.WEBPACK_ENV;
 var WebpackDevServer = require('webpack-dev-server');
 var path = require('path');
+var commonsPlugin = new webpack.optimize.CommonsChunkPlugin('bundle.js');
 
 var appName = 'app';
 var host = '0.0.0.0';
 var port = '9000';
 
-var commonsPlugin = new webpack.optimize.CommonsChunkPlugin('bundle.js')
-var plugins = [commonsPlugin], outputFile;
+var plugins = [], outputFile;
 
 if (env === 'build') {
-  plugins.push(new UglifyJsPlugin({
-    minimize: true
-  }));
+  plugins.push(new UglifyJsPlugin({ minimize: true }));
   outputFile = appName + '.min.js';
 } else {
   outputFile = appName + '.js';
 }
 
 var config = {
-  entry: {
-    index: './src/index.js',
-    vendors: ['react', 'react-dom']
-  },
+  entry: './src/index.js',
   devtool: 'source-map',
   output: {
     path: __dirname + '/lib',
-    filename: '[name].js',
+    filename: outputFile,
     publicPath: __dirname + '/example'
   },
   module: {
-    loaders: [{
-      test: /(\.jsx|\.js)$/,
-      loader: 'babel',
-      exclude: /(node_modules|bower_components)/
-    }, {
-      test: /(\.jsx|\.js)$/,
-      loader: "eslint-loader",
-      exclude: /node_modules/
-    }]
+    loaders: [
+      {
+        test: /(\.jsx|\.js)$/,
+        loader: 'babel',
+        exclude: /(node_modules|bower_components)/
+      },
+      {
+        test: /(\.jsx|\.js)$/,
+        loader: "eslint-loader",
+        exclude: /node_modules/
+      }
+    ]
   },
   resolve: {
     root: path.resolve('./src'),
@@ -54,7 +52,7 @@ if (env === 'dev') {
     contentBase: './example',
     hot: true,
     debug: true
-  }).listen(port, host, function(err, result) {
+  }).listen(port, host, function (err, result) {
     if (err) {
       console.log(err);
     }
