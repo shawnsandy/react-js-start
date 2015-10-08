5ca434500975ef370ab007d1c64828f236a01a37
Shawn Sandy <shawnsandy04@gmail.com>
2015-10-07 20:43:33 -0400

    Added Props / Validation

--- src/Component.js
+++ src/Component.js
@@ -4,7 +4,9 @@ import React from 'react';
  * Rect stateless components
  */
 
-const Button = ({ children = 'Sample Button' }) => (
+const Button = ({
+  children = 'Sample Button'
+}) => (
   <button>{children}</button>
 );
 
@@ -14,13 +16,21 @@ const Button = ({ children = 'Sample Button' }) => (
 
 export default class Component extends React.Component {
 
+  static defaultProps = {
+    title: 'Sample Component'
+  }
+
+  static propTypes = {
+    title: React.PropTypes.string
+  }
+
   render() {
     return (
       <aside className='hello-component'>
-        <h3>Sample Component</h3>
+        <h1>{ this.props.title }</h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
         </p>
-        <Button />
+        <Button>Button Name</Button>
       </aside>
     );
   }
